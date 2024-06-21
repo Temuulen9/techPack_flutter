b7407a6a0d403c31bce293b931dfb4a850b07901
Temuulen <9temuulen9@gmail.com>
2024-06-21 22:56:24 +0800

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:techpack_flutter/firebase_options.dart';
 import 'package:techpack_flutter/pages/login.dart';
 import 'package:techpack_flutter/pages/verify_otp.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
